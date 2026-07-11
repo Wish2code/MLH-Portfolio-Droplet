95bd4c554ada2c4bedae2a9c627c9684039f1e9b
Wish-VPS <dumbarimwengaatendwe@gmail.com>
2026-07-11 03:00:51 +0000

    Add a service to redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,8 +1,5 @@
 #!/bin/bash 
 
-#kill all tmux sessions 
-tmux kill-server 2>/dev/null
-
 #cd into my project folder
 cd ~/MLH-Portfolio-Droplet
 
@@ -15,7 +12,5 @@ source python3-virtualenv/bin/activate
 #install dependencies
 pip install -r requirements.txt
 
-#start tmux session
-tmux new-session -d -s flask \
-'cd ~/MLH-Portfolio-Droplet && source python3-virtualenv/bin/activate && flask run --host=0.0.0.0'
-
+#restart service
+systemctl restart myportfolio
